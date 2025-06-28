0eddc2aac30e187f40f8a96a6b46623ce9cc8548
sudo-Terry <contact.terrykim@gmail.com>
2025-06-29 07:19:34 +0900

    fix: `IllegalArgumentException` -> `ResourceNotFoundException`
    - Resource를 찾지 못하는 404 에러에 대해서 적합한 에러를 던져주자

--- src/main/java/team3/kummit/service/MemberService.java
+++ src/main/java/team3/kummit/service/MemberService.java
@@ -1,9 +1,12 @@
 package team3.kummit.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import team3.kummit.domain.Member;
+import team3.kummit.exception.ResourceNotFoundException;
 import team3.kummit.repository.MemberRepository;
 
 @Service
@@ -13,12 +16,12 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public Member findById(Long memberId){
-        return memberRepository.findById(memberId).orElseThrow(()-> new IllegalArgumentException("Not existing Member id"));
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new ResourceNotFoundException("사용자를 찾을 수 없습니다."));
     }
 
-    public Member findByName(String name){
+    public Optional<Member> findByName(String name){
         return memberRepository.findByName(name);
     }
-
 
 }
