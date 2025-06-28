42ebd8b7f50fd00c0d53dc6b8978a06a90b6c7ac
sudo-Terry <contact.terrykim@gmail.com>
2025-06-28 12:29:19 +0900

    Revert "feat: 밴드 좋아요 api 구현"
    This reverts commit bf887f6b09104a3b1b494a4f7bae56f430044a1c.

--- src/main/java/team3/kummit/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package team3.kummit.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
