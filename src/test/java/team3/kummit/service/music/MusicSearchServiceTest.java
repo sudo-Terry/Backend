05e3ec9faab19958d9c3d17221bea1c6cf6caa43
jay <rlaakswo0687@gmail.com>
2025-06-29 04:09:51 +0900

    Mock Data update

--- src/test/java/team3/kummit/service/music/MusicSearchServiceTest.java
+++ src/test/java/team3/kummit/service/music/MusicSearchServiceTest.java
@@ -1,10 +1,11 @@
 package team3.kummit.service.music;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
