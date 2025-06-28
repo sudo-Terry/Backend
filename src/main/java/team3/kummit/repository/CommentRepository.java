b7469cc918c4965fb280173b669f63a4c886c5df
sudo-Terry <contact.terrykim@gmail.com>
2025-06-28 14:42:57 +0900

    edit: code formatting

--- src/main/java/team3/kummit/repository/CommentRepository.java
+++ src/main/java/team3/kummit/repository/CommentRepository.java
@@ -1,6 +1,5 @@
 package team3.kummit.repository;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- src/main/java/team3/kummit/service/CommentService.java
+++ src/main/java/team3/kummit/service/CommentService.java
@@ -1,8 +1,6 @@
 package team3.kummit.service;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
