15b3f621671ba4bd3bd4eaae719157ec63ee93a8
sudo-Terry <contact.terrykim@gmail.com>
2025-06-28 16:44:36 +0900

    feat: 더미 데이터 생성 스크립트 코멘트 지원
    - 서버 실행시 생성되는 더미 데이터 목록에 코멘트 데이터들도 추가하자

--- src/main/java/team3/kummit/config/DataInitializer.java
+++ src/main/java/team3/kummit/config/DataInitializer.java
@@ -11,8 +11,10 @@ import org.springframework.context.annotation.Profile;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import team3.kummit.domain.Comment;
 import team3.kummit.domain.EmotionBand;
 import team3.kummit.domain.Member;
+import team3.kummit.repository.CommentRepository;
 import team3.kummit.repository.EmotionBandRepository;
 import team3.kummit.repository.MemberRepository;
 
@@ -23,6 +25,7 @@ public class DataInitializer {
 
     private final MemberRepository memberRepository;
     private final EmotionBandRepository emotionBandRepository;
+    private final CommentRepository commentRepository;
 
     @Bean
     @Profile("dev") // 개발 환경에서만 실행
@@ -130,9 +133,64 @@ public class DataInitializer {
 
             emotionBandRepository.saveAll(Arrays.asList(band1, band2, band3, band4, band5));
 
+            // 댓글 더미 데이터 생성
+            Comment comment1 = Comment.builder()
+                    .emotionBand(band1)
+                    .creator(member2)
+                    .creatorName(member2.getName())
+                    .comment("정말 기분 좋은 밴드네요! 저도 참여하고 싶어요 :)")
+                    .build();
+
+            Comment comment2 = Comment.builder()
+                    .emotionBand(band1)
+                    .creator(member3)
+                    .creatorName(member3.getName())
+                    .comment("좋은 일이 있다니 축하해요! 어떤 일이었나요?")
+                    .build();
+
+            Comment comment3 = Comment.builder()
+                    .emotionBand(band1)
+                    .creator(member1)
+                    .creatorName(member1.getName())
+                    .comment("다들 참여해주셔서 감사합니다!")
+                    .build();
+
+            Comment comment4 = Comment.builder()
+                    .emotionBand(band2)
+                    .creator(member1)
+                    .creatorName(member1.getName())
+                    .comment("쿠잇!!!!!!!!!!!!")
+                    .build();
+
+            Comment comment5 = Comment.builder()
+                    .emotionBand(band2)
+                    .creator(member3)
+                    .creatorName(member3.getName())
+                    .comment("슬픈 노래 추천해주세요 ㅜㅜ")
+                    .build();
+
+            Comment comment6 = Comment.builder()
+                    .emotionBand(band4)
+                    .creator(member1)
+                    .creatorName(member1.getName())
+                    .comment("평온한 마음으로 음악을 듣는 게 최고예요")
+                    .build();
+
+            Comment comment7 = Comment.builder()
+                    .emotionBand(band4)
+                    .creator(member2)
+                    .creatorName(member2.getName())
+                    .comment("오늘은 토요일이다...")
+                    .build();
+
+            commentRepository.saveAll(Arrays.asList(
+                    comment1, comment2, comment3, comment4, comment5, comment6, comment7
+            ));
+
             log.info("더미 데이터 초기화 완료!");
             log.info("생성된 멤버 수: {}", memberRepository.count());
             log.info("생성된 감정밴드 수: {}", emotionBandRepository.count());
+            log.info("생성된 댓글 수: {}", commentRepository.count());
         };
     }
 }
